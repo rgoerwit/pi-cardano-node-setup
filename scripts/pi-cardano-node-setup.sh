9c81b584ce187649c99d6c90c6769d46fb21d044
rgoerwit <achaar@goerwitz.com>
2021-04-15 14:40:06 -0500

    Set nginx-light to install

--- scripts/pi-cardano-node-setup.sh
+++ scripts/pi-cardano-node-setup.sh
@@ -332,7 +332,7 @@ $APTINSTALLER autoremove	1>> "$BUILDLOG" 2>&1
 # Install a bunch of necessary development and support packages
 $APTINSTALLER install apache2-utils aptitude autoconf automake bc bsdmainutils build-essential curl dialog emacs fail2ban g++ git \
 	gnupg gparted htop iproute2 jq libffi-dev libffi7 libgmp-dev libgmp10 libio-socket-ssl-perl libncursesw5 libpq-dev libsodium-dev libssl-dev \
-	libsystemd-dev libtinfo-dev libtinfo5 libtool libudev-dev libusb-1.0-0-dev make moreutils openssl nginx pkg-config python3 python3 \
+	libsystemd-dev libtinfo-dev libtinfo5 libtool libudev-dev libusb-1.0-0-dev make moreutils nginx nginx-light openssl pkg-config python3 python3 \
 	python3-pip librocksdb-dev netmask rocksdb-tools rsync secure-delete snapd sqlite sqlite3 systemd tcptraceroute tmux \
 	zlib1g-dev wcstools dos2unix ifupdown inetutils-traceroute libbz2-dev liblz4-dev libsnappy-dev libnuma-dev \
 	libqrencode4 libpam-google-authenticator    1>> "$BUILDLOG" 2>&1 \
