7e014ce891ce441716cfbbe3dd9d1dc6f3d6a5ca
rgoerwit <achaar@goerwitz.com>
2021-04-22 17:53:48 -0500

    Systemctl usage and heartbeat script improvemens

--- scripts/pi-cardano-heartbeat-failover.sh
+++ scripts/pi-cardano-heartbeat-failover.sh
@@ -17,20 +17,36 @@
 ###############################################################################
 
 
-[ -z "${EXTERNAL_IPV4_ADDRESS}" ] && EXTERNAL_IPV4_ADDRESS="$(dig +timeout=5 +short myip.opendns.com @resolver1.opendns.com 2> /dev/null | egrep -v '^;;' | tr -d '\r\n ')" 2> /dev/null
-[ -z "${EXTERNAL_IPV4_ADDRESS}" ] && EXTERNAL_IPV4_ADDRESS="$(host -4 myip.opendns.com resolver1.opendns.com 2> /dev/null | tail -1 | awk '{ print $(NF) }')" 2> /dev/null
-[ -z "${EXTERNAL_IPV6_ADDRESS}" ] && EXTERNAL_IPV6_ADDRESS="$(dig +timeout=5 +short -6 myip.opendns.com aaaa @resolver1.ipv6-sandbox.opendns.com 2> /dev/null | egrep -v '^;;' | tr -d '\r\n ')" 2> /dev/null
-[ -z "${EXTERNAL_IPV6_ADDRESS}" ] && EXTERNAL_IPV6_ADDRESS="$(host -6 myip.opendns.com resolver1.opendns.com 2> /dev/null | tail -1 | awk '{ print $(NF) }')" 2> /dev/null
-
-# If I am actually the parent, exit
+# If we are actually the parent, exit
+#
 for LOCALADDR in $(ip addr show | egrep '^[     ]*inet6?[       ]*' | awk '{ print $2 }' | sed 's|/[0-9.]*$||' | sort -u); do
-    if [ ".$LOCALADDR" = ".$PARENTADDR" ]; then
-        (systemctl status cardano-node | egrep -qi 'inactive.*dead') && exit 0
-        (systemctl status cardano-node | egrep -qi 'active.*unning') && exit 0
-        systemctl restart cardano-node
-    fi
+    [ ".$LOCALADDR" = ".$PARENTADDR" ] \
+        && exit 0
 done
+#
+[ -z "${EXTERNAL_IPV4_ADDRESS}" ] && EXTERNAL_IPV4_ADDRESS="$(dig +timeout=3 +short myip.opendns.com @resolver1.opendns.com 2> /dev/null | egrep -v '^;;' | tr -d '\r\n ')" 2> /dev/null
+[ -z "${EXTERNAL_IPV6_ADDRESS}" ] && EXTERNAL_IPV6_ADDRESS="$(dig +timeout=3 +short -6 myip.opendns.com aaaa @resolver1.ipv6-sandbox.opendns.com 2> /dev/null | egrep -v '^;;' | tr -d '\r\n ')" 2> /dev/null
+[ -z "${EXTERNAL_IPV4_ADDRESS}" ] && EXTERNAL_IPV4_ADDRESS="$(host -4 myip.opendns.com resolver1.opendns.com 2> /dev/null | tail -1 | awk '{ print $(NF) }')" 2> /dev/null
+[ -z "${EXTERNAL_IPV6_ADDRESS}" ] && EXTERNAL_IPV6_ADDRESS="$(host -6 myip.opendns.com resolver1.opendns.com 2> /dev/null | tail -1 | awk '{ print $(NF) }')" 2> /dev/null
+if [ ".$EXTERNAL_IPV4_ADDRESS" = ".$PARENTADDR" ] || [ ".$EXTERNAL_IPV6_ADDRESS" = ".$PARENTADDR" ]; then
+    exit 0
+fi
 
+# If we get to here, we are not the parent
+#
+[ -f "/lib/systemd/system/cardano-node.service"] && SYSTEMSTARTUPSCRIPT="/lib/systemd/system/cardano-node.service"
+[ -f "/etc/systemd/system/cardano-node.service"] && SYSTEMSTARTUPSCRIPT="/etc/systemd/system/cardano-node.service"
 
-sed -i
--e "s@^#* *NO_INTERNET_MODE=['\"]*N['\"]*@NO_INTERNET_MODE=\"\${NO_INTERNET_MODE:-Y}\"@" \
+# Parent is down, make us a block producer
+sed -i \
+    -e '/^[^#]*$/ s/^\([[:space:]]*ExecStart=.*\)[[:space:]]\(\(--[0-z]*-\(kes-key\|vrf-key\|operational-certificate\)[[:space:]]*[^[:space:]]*[[:space:]]*\)\{3\}.*\)$/\1 # \2/' \
+    "$SYSTEMSTARTUPSCRIPT"
+
+# Parent is OK (again), make us just a node
+sed -i \
+    -e '/^[[:space:]]*ExecStart/ s/ # / /' \
+    "$SYSTEMSTARTUPSCRIPT"
+
+if systemtcl is-active cardano-node 1> /dev/null; then
+    systemctl reload-or-restart cardano-node
+fi
