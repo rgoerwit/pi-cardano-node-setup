0d3f4e8ad526b974afe94bfdd60b9842f8890d7d
rgoerwit <achaar@goerwitz.com>
2021-04-26 21:57:12 -0500

    More fixups to deploy-as-systemd.sh

--- scripts/pi-cardano-node-setup.sh
+++ scripts/pi-cardano-node-setup.sh
@@ -1552,8 +1552,9 @@ if [ ".$DONT_OVERWRITE" != '.Y' ]; then
 	fi
 
 	debug "Rewriting Guild deploy-as-systemd.sh to ignore cnode.sh and use our system startup script"
-	 (echo -e '#!'"/usr/bin/env bash\nvname='cardano-node'\n"; sed '1,/^EOF"/d' < "${CARDANO_SCRIPTDIR}/deploy-as-systemd.sh") \
-	 	| sponge "${CARDANO_SCRIPTDIR}/deploy-as-systemd.sh"
+	(echo -e '#!'"/usr/bin/env bash\nvname='cardano-node'\nPARENT=\"\$(dirname \"\$0\")\"\n. \"\${PARENT}\"/env offline\n"; \
+		sed '1,/^EOF"/d' < "${CARDANO_SCRIPTDIR}/deploy-as-systemd.sh") \
+	 		| sponge "${CARDANO_SCRIPTDIR}/deploy-as-systemd.sh"
 fi
 [ -x "${CARDANO_SCRIPTDIR}/gLiveView.sh" ] \
     || err_exit 108 "$0: Can't find executable ${CARDANO_SCRIPTDIR}/gLiveView.sh; Guild scripts missing; aborting (drop -d option?)"
