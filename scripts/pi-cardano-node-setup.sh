bec65a11290e3b8683aecd01f8de7cfd570de1ac
rgoerwit <achaar@goerwitz.com>
2021-04-27 20:37:21 -0500

    Remove broken resolv.conf in chroot if needed

--- scripts/pi-cardano-node-setup.sh
+++ scripts/pi-cardano-node-setup.sh
@@ -457,6 +457,7 @@ ischroot || apt-mark unhold linux-image-generic linux-headers-generic cryptsetup
 if $APTINSTALLER update		1>> "$BUILDLOG" 2>&1; then
 	: yay 
 else
+	ischroot && find /etc -maxdepth 1 -xtype l -exec 'rm' -f {} \;
 	egrep -q '^nameserver 1\.1\.1\.1' /etc/resolv.conf || echo -e "nameserver 1.1.1.1\nnameserver 8.8.8.8" >> /etc/resolv.conf
 	$APTINSTALLER update --fix-missing || err_abort 21 "$0: Can't apply updates: '$APTINSTALLER update --fix-missing'; aborting"
 fi
