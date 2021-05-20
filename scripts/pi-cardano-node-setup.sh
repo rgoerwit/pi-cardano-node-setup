31a7fcd6b99bdaa5f150554139246e3eb4e925d4
rgoerwit <achaar@goerwitz.com>
2021-05-19 21:08:15 -0500

    Added comments

--- scripts/pi-cardano-node-setup.sh
+++ scripts/pi-cardano-node-setup.sh
@@ -806,7 +806,7 @@ else
 	_EOF
 	cat <<-_EOF > '/etc/fail2ban/filter.d/cardano.conf'
 		[Definition]
-		#Theses regex expressions capture nodes that are not on the latest fork and also
+		#Theses regex expressions capture nodes that are not on the latest fork and also; <HOST> is a regex macro
 		#nodes from other networks (testnets)
 		failregex = ^.*HardForkEncoderDisabledEra.*"address":"<HOST>:.*$
 			^.*version data mismatch.*"address":"<HOST>:.*$
@@ -888,7 +888,7 @@ else
 	debug "Prometheus (via nginx) credentials: username, stats; pass, $(cat ${PROMETHEUS_DIR}/nginx-passwd-cleartext.txt | tail -1 | sed 's/\n$//')"
 	[ -d "$NGINX_CONF_DIR" ] || NGINX_CONF_DIR='/etc/nginx/conf.d'
 	cat <<- _EOF > "$NGINX_CONF_DIR/nginx-${EXTERNAL_HOSTNAME}.conf" 
-		limit_req_zone \$binary_remote_addr zone=one:10m rate=1r/s;
+		limit_req_zone \$binary_remote_addr zone=one:3m rate=1r/s;
 		server {
 		    listen              $EXTERNAL_PROMETHEUS_PORT ssl;
 		    server_name         example.com;
